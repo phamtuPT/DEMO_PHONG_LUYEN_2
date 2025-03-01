c0161c4e3d6b2c484520f2927917dc971f6bdb74
Phạm Tú <107177749+phamtuPT@users.noreply.github.com>
2025-03-01 21:10:26 +0700

    script.js

--- script.js
+++ script.js
@@ -63,7 +63,8 @@ document.addEventListener("DOMContentLoaded", function () {
       "635718": { "name": "nguyễn tiến đạt", "examCode": "ASQORN" },
       "778508": { "name": "Đỗ Anh Quân", "examCode": "AT1JVU" },
       "422207": { "name": "Nguyễn Ninh", "examCode": "A8ZF57" },
-      "359907": { "name": "Nguyễn Mạnh Chung ", "examCode": "AUSG5E" }
+      "359907": { "name": "Nguyễn Mạnh Chung ", "examCode": "AUSG5E" },
+      "352007": { "name": "Đoàn Phước Long", "examCode": "AIF3YM" }
   };
 
  
